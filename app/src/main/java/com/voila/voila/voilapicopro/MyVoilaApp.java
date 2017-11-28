a09b88b120a2237e67f3a25095a7540194920b0b
daniel <daniel_amar@berkeley.edu>
2017-11-28 15:48:45 -0800

    update

--- app/src/main/java/com/voila/voila/voilapicopro/MyVoilaApp.java
+++ app/src/main/java/com/voila/voila/voilapicopro/MyVoilaApp.java
@@ -88,6 +88,7 @@ public class MyVoilaApp extends Application {
     public void setQuestion(String questionValue) {
         this.question= questionValue;
     }
+
     public String getQuestionExtra() {
         return questionExtra;
     }
